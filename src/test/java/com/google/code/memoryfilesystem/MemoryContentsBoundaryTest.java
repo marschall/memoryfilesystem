c11d01b8c1ec6b7aafc6d073b417d06bfb1d32eb
Philippe Marschall <philippe.marschall@gmail.com>
2011-10-18 07:13:37 +0000

    - extend tests

--- src/test/java/com/google/code/memoryfilesystem/MemoryContentsBoundaryTest.java
+++ src/test/java/com/google/code/memoryfilesystem/MemoryContentsBoundaryTest.java
@@ -19,27 +19,35 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class MemoryContentsBoundaryTest {
-
-  private final int twoWrite;
+  
+  private final int initialOffset;
 
   private final int initialBlocks;
+  
+  private final int toWrite;
 
   private MemoryContents contents;
 
-  public MemoryContentsBoundaryTest(int initialBlocks, int twoWrite) {
-    this.twoWrite = twoWrite;
+  public MemoryContentsBoundaryTest(int initialOffset, int initialBlocks, int twoWrite) {
+    this.initialOffset = initialOffset;
+    this.toWrite = twoWrite;
     this.initialBlocks = initialBlocks;
   }
 
   @Parameters
   public static List<Object[]> parameters() {
+    int blockSize = MemoryContents.BLOCK_SIZE;
     List<Object[]> parameters = new ArrayList<>();
-    for (int initialBlockSize = 0; initialBlockSize < 10; initialBlockSize++) {
-      for (int multiplier = 1; multiplier < 20; multiplier++) {
-        int offset = MemoryContents.BLOCK_SIZE * multiplier;
-        parameters.add(new Object[]{initialBlockSize, offset - 1});
-        parameters.add(new Object[]{initialBlockSize, offset});
-        parameters.add(new Object[]{initialBlockSize, offset + 1});
+    for (int initialOffset : new int[]{0, blockSize - 2, blockSize - 1, blockSize, blockSize + 1, blockSize + 2}) {
+      for (int initialBlockSize = 0; initialBlockSize < 10; initialBlockSize++) {
+        for (int multiplier = 1; multiplier < 5; multiplier++) {
+          int offset = blockSize * multiplier;
+          parameters.add(new Object[]{initialOffset, initialBlockSize, offset - 2});
+          parameters.add(new Object[]{initialOffset, initialBlockSize, offset - 1});
+          parameters.add(new Object[]{initialOffset, initialBlockSize, offset});
+          parameters.add(new Object[]{initialOffset, initialBlockSize, offset + 1});
+          parameters.add(new Object[]{initialOffset, initialBlockSize, offset + 2});
+        }
       }
     }
     return parameters;
@@ -51,35 +59,81 @@ public class MemoryContentsBoundaryTest {
   }
 
   @Test
-  public void write() throws IOException {
-    byte[] data = new byte[this.twoWrite];
+  public void boundaryWrapping() throws IOException {
+    SeekableByteChannel channel = this.contents.newChannel(true, true);
+    byte[] initial = new byte[this.initialOffset];
+    channel.write(ByteBuffer.wrap(initial));
+    
+    
+    byte[] data = new byte[this.toWrite];
     data[data.length - 1] = 9;
     data[data.length - 2] = 8;
     data[data.length - 3] = 7;
+    data[2] = 3;
+    data[1] = 2;
+    data[0] = 1;
     ByteBuffer src = ByteBuffer.wrap(data);
 
-    SeekableByteChannel channel = this.contents.newChannel(true, true);
     channel.write(src);
-    assertEquals(twoWrite, channel.size());
-    assertEquals(twoWrite, channel.position());
+    assertEquals(toWrite + this.initialOffset, channel.size());
+    assertEquals(toWrite + this.initialOffset, channel.position());
 
-    byte[] lastOne = new byte[1];
-    ByteBuffer dst = ByteBuffer.wrap(lastOne);
+    // one element tests
+    
+    // read last element
+    byte[] oneElement = new byte[1];
+    ByteBuffer dst = ByteBuffer.wrap(oneElement);
     channel.position(channel.size() - 1L);
     assertEquals(1, channel.read(dst));
-    assertArrayEquals(new byte[]{9}, lastOne);
+    assertArrayEquals(new byte[]{9}, oneElement);
+    
+    // read first element
+    dst.rewind();
+    long startOfWrite = channel.size() - (long) toWrite;
+    channel.position(startOfWrite);
+    assertEquals(1, channel.read(dst));
+    assertArrayEquals(new byte[]{1}, oneElement);
+    
+    // two element tests
 
-    byte[] lastTwo = new byte[2];
-    dst = ByteBuffer.wrap(lastTwo);
+    // read last two elements
+    byte[] twoElements = new byte[2];
+    dst = ByteBuffer.wrap(twoElements);
     channel.position(channel.size() - 2L);
     assertEquals(2, channel.read(dst));
-    assertArrayEquals(new byte[]{8, 9}, lastTwo);
+    assertArrayEquals(new byte[]{8, 9}, twoElements);
+    
+    // read first two elements
+    dst.rewind();
+    channel.position(startOfWrite);
+    assertEquals(2, channel.read(dst));
+    assertArrayEquals(new byte[]{1, 2}, twoElements);
 
-    byte[] lastThree = new byte[3];
-    dst = ByteBuffer.wrap(lastThree);
+    
+    // read last three elements
+    byte[] threeElements = new byte[3];
+    dst = ByteBuffer.wrap(threeElements);
     channel.position(channel.size() - 3L);
     assertEquals(3, channel.read(dst));
-    assertArrayEquals(new byte[]{7, 8, 9}, lastThree);
+    assertArrayEquals(new byte[]{7, 8, 9}, threeElements);
+    
+    // read first two elements
+    dst.rewind();
+    channel.position(startOfWrite);
+    assertEquals(3, channel.read(dst));
+    assertArrayEquals(new byte[]{1, 2, 3}, threeElements);
+    
+    // read the full data back
+    byte[] readBack = new byte[this.toWrite];
+    dst = ByteBuffer.wrap(readBack);
+    channel.position(channel.size() - (long) this.toWrite);
+    assertEquals(this.toWrite, channel.read(dst));
+    assertEquals(1, readBack[0]);
+    assertEquals(2, readBack[1]);
+    assertEquals(3, readBack[2]);
+    assertEquals(7, readBack[readBack.length - 3]);
+    assertEquals(8, readBack[readBack.length - 2]);
+    assertEquals(9, readBack[readBack.length - 1]);
   }
 
 }
