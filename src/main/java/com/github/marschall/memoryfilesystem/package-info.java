3c348c0bcfc77189bdeb86e00179c98ca9e4e643
Philippe Marschall <philippe.marschall@gmail.com>
2012-09-25 05:55:58 +0000

    - update doc

--- src/main/java/com/github/marschall/memoryfilesystem/package-info.java
+++ src/main/java/com/github/marschall/memoryfilesystem/package-info.java
@@ -3,7 +3,8 @@
  * <a href="http://jcp.org/en/jsr/detail?id=203">JSR-203</a> file system.
  *
  * <p>All public classes in this package except
- * {@link com.github.marschall.memoryfilesystem.MemoryFileSystemProvider} can be
+ * {@link com.github.marschall.memoryfilesystem.MemoryFileSystemProvider}
+ * and {@link com.github.marschall.memoryfilesystem.Activator} can be
  * used by client code.</p>
  * 
  */
