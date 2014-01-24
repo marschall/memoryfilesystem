ca195e26fa7af54266ab4d497268b37f8e151e02
Philippe Marschall <philippe.marschall@gmail.com>
2014-01-24 21:44:20 +0100

    Fix link in package-info.java
    This should make the project build on Java 8.

--- src/main/java/com/github/marschall/memoryfilesystem/package-info.java
+++ src/main/java/com/github/marschall/memoryfilesystem/package-info.java
@@ -4,7 +4,6 @@
  *
  * <p>All public classes in this package except
  * {@link com.github.marschall.memoryfilesystem.MemoryFileSystemProvider}
- * and {@link com.github.marschall.memoryfilesystem.Activator} can be
  * used by client code.</p>
  * 
  */
