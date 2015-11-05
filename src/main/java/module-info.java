6bdc8c523ad496376e6c80131742638212d0224c
Philippe Marschall <philippe.marschall@gmail.com>
2015-11-05 18:38:40 +0100

    Move jigsaw support to branch. Needs more testing.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.github.marschall.memoryfilesystem {
-
-  requires /* optional */ java.annotations.common;
-  exports com.github.marschall.memoryfilesystem;
-  provides java.nio.file.spi.FileSystemProvider
-  with com.github.marschall.memoryfilesystem.MemoryFileSystemProvider;
-
-}
