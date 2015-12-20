5d15685c91d7d39854e5b02aeb11c8dbc51654cd
Philippe Marschall <philippe.marschall@gmail.com>
2015-12-20 15:34:11 +0100

    improve javadoc

--- src/main/java/com/github/marschall/memoryfilesystem/CurrentGroup.java
+++ src/main/java/com/github/marschall/memoryfilesystem/CurrentGroup.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.attribute.GroupPrincipal;
 import java.util.concurrent.Callable;
 
+/**
+ * Provides access to the group of the current user.
+ */
 public final class CurrentGroup {
 
   private CurrentGroup() {
@@ -12,6 +15,14 @@ public final class CurrentGroup {
 
   private static final ThreadLocal<GroupPrincipal> GROUP = new ThreadLocal<>();
 
+  /**
+   * Sets the current group for a certain period.
+   *
+   * @param group the group to use
+   * @param task during this task the given group will be used, will be called
+   *  immediately by the current thread
+   * @return what the task returned
+   */
   public static <V> V useDuring(GroupPrincipal group, GroupTask<V> task) throws IOException {
     GroupPrincipal previous = GROUP.get();
     try {
@@ -30,10 +41,22 @@ public final class CurrentGroup {
     return GROUP.get();
   }
 
+  /**
+   * Functional interface for a task during which a certain group should be used.
+   *
+   * @param <V> the type of the return value
+   */
   public interface GroupTask<V> extends Callable<V> {
 
+    /**
+     * Executes the task.
+     *
+     * @return the return value of the task
+     * @throws IOException if any of the code in the task throws an
+     *  {@link IOException}
+     */
     @Override
-    public V call() throws IOException;
+    V call() throws IOException;
 
   }
 
--- src/main/java/com/github/marschall/memoryfilesystem/CurrentUser.java
+++ src/main/java/com/github/marschall/memoryfilesystem/CurrentUser.java
@@ -3,7 +3,9 @@ package com.github.marschall.memoryfilesystem;
 import java.io.IOException;
 import java.nio.file.attribute.UserPrincipal;
 import java.util.concurrent.Callable;
-
+/**
+ * Provides access to the current user.
+ */
 public final class CurrentUser {
 
   private CurrentUser() {
@@ -12,6 +14,14 @@ public final class CurrentUser {
 
   private static final ThreadLocal<UserPrincipal> USER = new ThreadLocal<>();
 
+  /**
+   * Sets the current user for a certain period.
+   *
+   * @param user the user to use
+   * @param task during this task the given user will be used, will be called
+   *  immediately by the current thread
+   * @return what the task returned
+   */
   public static <V> V useDuring(UserPrincipal user, UserTask<V> task) throws IOException {
     UserPrincipal previous = USER.get();
     try {
@@ -30,11 +40,22 @@ public final class CurrentUser {
     return USER.get();
   }
 
-
+  /**
+   * Functional interface for a task during which a certain user should be used.
+   *
+   * @param <V> the type of the return value
+   */
   public interface UserTask<V> extends Callable<V> {
 
+    /**
+     * Executes the task.
+     *
+     * @return the return value of the task
+     * @throws IOException if any of the code in the task throws an
+     *  {@link IOException}
+     */
     @Override
-    public V call() throws IOException;
+    V call() throws IOException;
 
   }
 
