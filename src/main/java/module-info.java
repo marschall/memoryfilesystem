94e87b23e88c352b858d9d5c4111820ed7cd6d41
Philippe Marschall <philippe.marschall@gmail.com>
2016-11-11 22:06:04 +0100

    Provide Jigsaw Module
    Initial work on Jigsaw module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.github.marschall.memoryfilesystem {
+
+  requires /* static */ java.annotations.common;
+
+  exports com.github.marschall.memoryfilesystem;
+
+  provides java.nio.file.spi.FileSystemProvider
+  with com.github.marschall.memoryfilesystem.MemoryFileSystemProvider;
+
+}
