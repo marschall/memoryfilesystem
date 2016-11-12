fca5ad3f87c737eaa2c1df1005f96c5c7c74f929
Philippe Marschall <philippe.marschall@gmail.com>
2016-11-12 10:32:06 +0100

    add link

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,10 @@
 module com.github.marschall.memoryfilesystem {
 
+  /*
+   * Should work be apparently isn't implemented yet
+   *
+   * http://openjdk.java.net/projects/jigsaw/spec/issues/#CompileTimeDependences
+   */
   requires /* static */ java.annotations.common;
 
   exports com.github.marschall.memoryfilesystem;
