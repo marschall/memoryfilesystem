86f1c3f9446a2f6ce1c603161ffba988f323fd07
Philippe Marschall <philippe.marschall@gmail.com>
2017-05-23 20:01:35 +0200

    Try Jigsaw again
    part of #82
    see if this works on travis

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,6 @@
 module com.github.marschall.memoryfilesystem {
 
-  /*
-   * Should work be apparently isn't implemented yet
-   *
-   * http://openjdk.java.net/projects/jigsaw/spec/issues/#CompileTimeDependences
-   */
-  requires /* static */ java.annotations.common;
+  requires static java.xml.ws.annotation;
 
   exports com.github.marschall.memoryfilesystem;
 
