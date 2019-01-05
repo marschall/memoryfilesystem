f9f5b1f973eb6c5c99c09fab52694e5f6883bc23
Stefan Ferstl <st.ferstl@gmail.com>
2019-01-05 23:00:21 +0100

    Add RootExtension as JUnit5 equivalent of RootRule

---
+++ src/test/java/com/github/marschall/memoryfilesystem/RootExtension.java
@@ -0,0 +1,34 @@
+package com.github.marschall.memoryfilesystem;
+
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import static com.github.marschall.memoryfilesystem.Constants.SAMPLE_ENV;
+import static com.github.marschall.memoryfilesystem.Constants.SAMPLE_URI;
+
+public class RootExtension implements BeforeEachCallback, AfterEachCallback {
+
+  private Path root;
+  private FileSystem fileSystem;
+
+
+  Path getRoot() {
+    return this.root;
+  }
+
+  @Override
+  public void beforeEach(ExtensionContext context) throws Exception {
+    this.fileSystem = FileSystems.newFileSystem(SAMPLE_URI, SAMPLE_ENV);
+    this.root = fileSystem.getRootDirectories().iterator().next();
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) throws Exception {
+    if (this.fileSystem != null) {
+      this.fileSystem.close();
+    }
+  }
+}
